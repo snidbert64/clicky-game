6203668239071b82181b10bf8f8e4501b5b982d5
snidbert64 <zachalotz@gmail.cp>
2019-05-08 20:55:39 -0500

    Updates

---
+++ precache-manifest.f02fee416f54b2a0fe92bc08dcc064c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b4342ba0fbd35d637be0",
+    "url": "/static/js/main.594100d1.chunk.js"
+  },
+  {
+    "revision": "8795d4af043ddfe37042",
+    "url": "/static/js/2.7cb9f535.chunk.js"
+  },
+  {
+    "revision": "b4342ba0fbd35d637be0",
+    "url": "/static/css/main.b635d565.chunk.css"
+  },
+  {
+    "revision": "d665187a0345caa47190aef2ce84f4cf",
+    "url": "/index.html"
+  }
+];
